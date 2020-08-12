25fb5483e336fd1237d3dd76042d7ab3e03651bb
Praveen Mathew <praveen.mathew92@outlook.com>
2020-08-12 13:54:39 +0530

    cat clone with lines flag

--- ch05/exercises/ex04.go
+++ ch05/exercises/ex04.go
@@ -8,10 +8,19 @@ import (
 
 func main () {
 	path := os.Args[len(os.Args) - 1]
+	shouldPrintLines := os.Args[1] == "-n"
 	file, _ := os.Open(path)
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	if shouldPrintLines {
+		lineNumber := 1
+		for scanner.Scan() {
+			lineNumber+=1
+			fmt.Printf("%d %s\n", lineNumber, scanner.Text())
+		}
+	} else {
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
 	}
 }
